6ea1439c09da25d7f31eed40be5fb00d9bb8e09b
luisp <luisp@JUNIOR-NOTE>
2017-09-11 01:30:23 -0300

    Migração parcial do formulário cadastro para thymeleaf

--- src/main/java/projetofitapp/controller/MainController.java
+++ src/main/java/projetofitapp/controller/MainController.java
@@ -8,9 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String listUploadedFiles() throws IOException {
+	@GetMapping("/")
+	public String index() throws IOException {
+		
+		return "index";
+	}
+	
+	@GetMapping("/cadastro")
+	public String formularioCadastro() throws IOException {
+		
+		return "formularioCadastro";
+	}
 
-        return "index";
-    }
 }
